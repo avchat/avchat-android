6624078c2a0492c1d2b385fa1f42f29e1a70729d
ccpony86 <19461718@qq.com>
2014-01-19 22:30:54 +0800

    init

--- src/com/ccpony/avchat/peerconnection/PCJS2Java.java
+++ src/com/ccpony/avchat/peerconnection/PCJS2Java.java
@@ -1,62 +1,6 @@
 package com.ccpony.avchat.peerconnection;
 
-import android.webkit.JavascriptInterface;
 
 public class PCJS2Java {
-	@JavascriptInterface
-	public void addIceCandidate(){
-		
-	}
-
-	@JavascriptInterface
-	public void addStream(){
-	}
-
-	@JavascriptInterface
-	public void close(){
-	}
-
-	@JavascriptInterface
-	public void createAnswer(){
-	}
-
-	@JavascriptInterface
-	public void createDataChannel(){
-	}
-
-	@JavascriptInterface
-	public void createOffer(){
-	}
-
-	@JavascriptInterface
-	public void getLocalStreams(){
-	}
-
-	@JavascriptInterface
-	public void getRemoteStreams(){
-	}
-
-	@JavascriptInterface
-	public void getStats(){
-	}
-
-	@JavascriptInterface
-	public void getStreamById(){
-	}
-
-	@JavascriptInterface
-	public void removeStream(){
-	}
-
-	@JavascriptInterface
-	public void setLocalDescription(){
-	}
-
-	@JavascriptInterface
-	public void setRemoteDescription(){
-	}
-
-	@JavascriptInterface
-	public void updateIce(){
-	}
+	
 }
--- src/com/ccpony/avchat/peerconnection/PCJava2JS.java
+++ src/com/ccpony/avchat/peerconnection/PCJava2JS.java
@@ -1,20 +1,29 @@
 package com.ccpony.avchat.peerconnection;
 
+import java.util.List;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.webrtc.DataChannel;
 import org.webrtc.IceCandidate;
+import org.webrtc.MediaConstraints;
 import org.webrtc.MediaStream;
 import org.webrtc.PeerConnection;
+import org.webrtc.PeerConnectionFactory;
 import org.webrtc.SdpObserver;
 import org.webrtc.SessionDescription;
+import org.webrtc.StatsObserver;
+import org.webrtc.StatsReport;
 import org.webrtc.VideoCapturer;
 import org.webrtc.VideoRenderer;
+import org.webrtc.VideoSource;
 import org.webrtc.VideoRenderer.I420Frame;
+import org.webrtc.VideoTrack;
 
 import android.app.Activity;
 import android.graphics.Point;
 import android.util.Log;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.widget.Toast;
 
@@ -26,11 +35,20 @@ public class PCJava2JS {
 	private static final String TAG = "PCJava2JS";
 	private Toast logToast;
 	private VideoStreamsView vsv;
-	
+	private PeerConnectionFactory factory;
+	private PeerConnection pc;
+	private final PCObserver pcObserver = new PCObserver();
+	private final SDPObserver sdpObserver = new SDPObserver();
+	private final Boolean[] quit = new Boolean[] { false };
+	private VideoSource videoSource;
+	public MediaConstraints pcConstraints;
+	public MediaConstraints videoConstraints;
+	List<PeerConnection.IceServer> iceServers = null;
+
 	public PCJava2JS(WebView js_runtime, Activity activity) {
 		this.js_runtime = js_runtime;
 		this.activity = activity;
-		
+
 		Point displaySize = new Point();
 		activity.getWindowManager().getDefaultDisplay().getSize(displaySize);
 		vsv = new VideoStreamsView(activity, displaySize);
@@ -38,18 +56,18 @@ public class PCJava2JS {
 	}
 
 	public void onicecandidate(String param1) {
-		js_runtime.loadUrl("javascript:peerconnection.onicecandidate("+ param1 +")"); 
+		js_runtime.loadUrl("javascript:peerconnection.onicecandidate(" + param1
+				+ ")");
 	}
-		
+
 	public void cb_createOffer() {
 		js_runtime.loadUrl("javascript:peerconnection.cb_createOffer()");
 	}
-	
+
 	public void cb_createAnswer() {
 		js_runtime.loadUrl("javascript:peerconnection.cb_createAnswer()");
-	}	
-	
-	
+	}
+
 	// Log |msg| and Toast about it.
 	private void logAndToast(String msg) {
 		Log.d(TAG, msg);
@@ -59,7 +77,7 @@ public class PCJava2JS {
 		logToast = Toast.makeText(activity, msg, Toast.LENGTH_SHORT);
 		logToast.show();
 	}
-	
+
 	// Put a |key|->|value| mapping in |json|.
 	private static void jsonPut(JSONObject json, String key, Object value) {
 		try {
@@ -68,11 +86,12 @@ public class PCJava2JS {
 			throw new RuntimeException(e);
 		}
 	}
-		
+
 	private VideoCapturer getVideoCapturer() {
 		String[] cameraFacing = { "front", "back" };
 		int[] cameraIndex = { 0, 1 };
 		int[] cameraOrientation = { 0, 90, 180, 270 };
+		
 		for (String facing : cameraFacing) {
 			for (int index : cameraIndex) {
 				for (int orientation : cameraOrientation) {
@@ -88,19 +107,19 @@ public class PCJava2JS {
 		}
 		throw new RuntimeException("Failed to open capturer");
 	}
-	
+
 	// Poor-man's assert(): die with |msg| unless |condition| is true.
 	private static void abortUnless(boolean condition, String msg) {
 		if (!condition) {
 			throw new RuntimeException(msg);
 		}
 	}
-	
+
 	// Send |json| to the underlying AppEngine Channel.
 	private void sendMessage(JSONObject json) {
-		
+
 	}
-	
+
 	// Implementation detail: bridge the VideoRenderer.Callbacks interface to
 	// the
 	// VideoStreamsView implementation.
@@ -128,7 +147,7 @@ public class PCJava2JS {
 			view.queueFrame(stream, frame);
 		}
 	}
-	
+
 	// Implementation detail: observe ICE & stream changes and react
 	// accordingly.
 	private class PCObserver implements PeerConnection.Observer {
@@ -154,7 +173,8 @@ public class PCJava2JS {
 		@Override
 		public void onIceConnectionChange(
 				PeerConnection.IceConnectionState newState) {
-			js_runtime.loadUrl("javascript:peerconnection.oniceconnectionstatechange()");
+			js_runtime
+					.loadUrl("javascript:peerconnection.oniceconnectionstatechange()");
 		}
 
 		@Override
@@ -165,8 +185,9 @@ public class PCJava2JS {
 		@Override
 		public void onAddStream(final MediaStream stream) {
 			js_runtime.loadUrl("javascript:peerconnection.onaddstream()");
-			abortUnless(stream.audioTracks.size() <= 1
-					&& stream.videoTracks.size() <= 1,
+			abortUnless(
+					stream.audioTracks.size() <= 1
+							&& stream.videoTracks.size() <= 1,
 					"Weird-looking stream: " + stream);
 			if (stream.videoTracks.size() == 1) {
 				stream.videoTracks.get(0).addRenderer(
@@ -184,11 +205,11 @@ public class PCJava2JS {
 		@Override
 		public void onDataChannel(final DataChannel dc) {
 			throw new RuntimeException(
-					"AppRTC doesn't use data channels, but got: "
-							+ dc.label() + " anyway!");
+					"AppRTC doesn't use data channels, but got: " + dc.label()
+							+ " anyway!");
 		}
 	}
-	
+
 	// Implementation detail: handle offer creation/signaling and answer
 	// setting,
 	// as well as adding remote ICE candidates once the answer SDP is set.
@@ -213,5 +234,117 @@ public class PCJava2JS {
 
 		private void drainRemoteCandidates() {
 		}
+	}
+
+	public void create_pc() {
+		factory = new PeerConnectionFactory();
+
+		MediaConstraints pcConstraints = null;
+		pcConstraints.optional.add(new MediaConstraints.KeyValuePair(
+				"RtpDataChannels", "true"));
+
+		
+		pc = factory
+				.createPeerConnection(iceServers, pcConstraints, pcObserver);
+
+		final PeerConnection finalPC = pc;
+		final Runnable repeatedStatsLogger = new Runnable() {
+			public void run() {
+				synchronized (quit[0]) {
+					if (quit[0]) {
+						return;
+					}
+					final Runnable runnableThis = this;
+					boolean success = finalPC.getStats(new StatsObserver() {
+						public void onComplete(StatsReport[] reports) {
+							for (StatsReport report : reports) {
+								Log.d(TAG, "Stats: " + report.toString());
+							}
+							vsv.postDelayed(runnableThis, 10000);
+						}
+					}, null);
+					if (!success) {
+						throw new RuntimeException(
+								"getStats() return false!");
+					}
+				}
+			}
+		};
+		vsv.postDelayed(repeatedStatsLogger, 10000);		
+	}
+	
+	public void getusermedia() {
+		logAndToast("Creating local video source...");
+		MediaStream lMS = factory.createLocalMediaStream("ARDAMS");
+		VideoCapturer capturer = getVideoCapturer();
+		videoSource = factory.createVideoSource(capturer, videoConstraints);
+		VideoTrack videoTrack = factory.createVideoTrack("ARDAMSv0",
+				videoSource);
+		videoTrack.addRenderer(new VideoRenderer(new VideoCallbacks(vsv,
+				VideoStreamsView.Endpoint.LOCAL)));
+		lMS.addTrack(videoTrack);
+		lMS.addTrack(factory.createAudioTrack("ARDAMSa0"));
+		pc.addStream(lMS, new MediaConstraints());
+	}
+	
+	public void create_player() {
+		
+	}
+
+	@JavascriptInterface
+	public void addIceCandidate() {
+
+	}
+
+	@JavascriptInterface
+	public void addStream() {
+	}
+
+	@JavascriptInterface
+	public void close() {
+	}
+
+	@JavascriptInterface
+	public void createAnswer() {
+	}
+
+	@JavascriptInterface
+	public void createDataChannel() {
+	}
+
+	@JavascriptInterface
+	public void createOffer() {
+	}
+
+	@JavascriptInterface
+	public void getLocalStreams() {
+	}
+
+	@JavascriptInterface
+	public void getRemoteStreams() {
+	}
+
+	@JavascriptInterface
+	public void getStats() {
+	}
+
+	@JavascriptInterface
+	public void getStreamById() {
+	}
+
+	@JavascriptInterface
+	public void removeStream() {
+	}
+
+	@JavascriptInterface
+	public void setLocalDescription() {
+	}
+
+	@JavascriptInterface
+	public void setRemoteDescription() {
+	}
+
+	@JavascriptInterface
+	public void updateIce() {
 	}
 }
