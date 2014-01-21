41ef5044786d7dc340b950a9f91e283726b36c68
ccpony86 <19461718@qq.com>
2014-01-21 17:36:44 +0800

    update pcmanager

--- src/com/ccpony/avchat/peerconnection/PCManager.java
+++ src/com/ccpony/avchat/peerconnection/PCManager.java
@@ -2,20 +2,35 @@ package com.ccpony.avchat.peerconnection;
 
 import java.util.HashMap;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.JsonReader;
 import android.webkit.JavascriptInterface;
 
 public class PCManager {
 	HashMap<String, PCWrapper> pc_map = new HashMap<String, PCWrapper>();
 	
 	@JavascriptInterface
-	public void create_pc(String pc_id) {
+	public void add_pc(String pc_id) {
 		PCWrapper pc_wrapper = new PCWrapper(null, null);
 		pc_map.put(pc_id, pc_wrapper);
 	}
 	
 	@JavascriptInterface
-	public void add_stream(String pc_id) {
-		PCWrapper pc_wrapper = pc_map.get(pc_id);
-		pc_wrapper.addStream();
+	public void remove_pc(String pc_id) {
+		pc_map.remove(pc_id);
 	}
+	
+//	@JavascriptInterface
+//	public void add_stream(String pc_id, JSONObject obj) {
+//		PCWrapper pc_wrapper = pc_map.get(pc_id);
+//		String param = obj.toString();
+//		try {
+//			JSONObject res = new JSONObject(param);
+//		} catch (JSONException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
 }
